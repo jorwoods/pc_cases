f13fa03f21b2f3f758c5fa23ad5d03a9fbd70e26
Jordan Woods <lenluin@gmail.com>
2021-12-26 10:54:53 -0600

    Skip middleware

--- pc_cases/middlewares.py
+++ pc_cases/middlewares.py
@@ -78,7 +78,7 @@ class PcCasesDownloaderMiddleware:
         # - or return a Request object
         # - or raise IgnoreRequest: process_exception() methods of
         #   installed downloader middleware will be called
-        return request
+        return None
 
     def process_response(self, request, response, spider):
         # Called with the response returned from the downloader.
